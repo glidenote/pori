dd1f16fec5d57f75aa83a13906fcd6d179cb5aaf
Akira Maeda <glidenote@gmail.com>
2012-06-29 19:27:10 +0900

    add delete function
    now not working delete function

--- lib/pori/commands.rb
+++ lib/pori/commands.rb
@@ -25,7 +25,7 @@ module Pori
     end
 
     def create
-      repo = new_repo_name
+      repo = repo_name
 
       puts "curl --request POST --user #{@username}:#{@password.chars.map{|c| '*'}.join} https://api.bitbucket.org/1.0/repositories/ --data name=#{repo} --data scm=git --data is_private=true"
       system "curl --request POST --user #{@username}:#{@password} https://api.bitbucket.org/1.0/repositories/ --data name=#{repo} --data scm=git --data is_private=true"
@@ -33,8 +33,17 @@ module Pori
       system "git remote add origin ssh://git@bitbucket.org/#{@username}/#{repo}.git"
     end
 
+    def delete
+      repo = repo_name
+
+      puts "Do you want delete https://bitbucket.org/#{@username}/#{repo}? [y/N]"
+
+      puts "curl --request DELETE --user #{@username}:#{@password.chars.map{|c| '*'}.join} https://api.bitbucket.org/1.0/repositories/#{@username}/#{repo}"
+      system "curl --request DELETE --user #{@username}:#{@password.chars.map{|c| '*'}.join} https://api.bitbucket.org/1.0/repositories/#{@username}/#{repo}"
+    end
+
     private
-    def new_repo_name
+    def repo_name
       raise Exception, "Not a git repository" unless is_git_repo?
       Dir.pwd.split('/')[-1]
     end
